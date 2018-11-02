511a382e0e457b6876289d8179e108c6408ce7ec
ahossain2677 <43207698+ahossain2677@users.noreply.github.com>
2018-11-02 08:23:51 -0400

    Create chatbox2.java

---
+++ src/com/company/chatbox2.java
@@ -0,0 +1,208 @@
+import java.util.Random;
+import java.util.Scanner;
+
+
+public class ChatBot2
+{
+	
+	int emotion = 0;
+
+
+
+	public void chatLoop(String statement)
+	{
+		Scanner in = new Scanner (System.in);
+		System.out.println (getGreeting());
+
+
+		while (!statement.equals("Bye"))
+		{
+
+
+			statement = in.nextLine();
+			//getResponse handles the user reply
+			System.out.println(getResponse(statement));
+
+
+		}
+
+	}
+	
+	public String getGreeting()
+	{
+		return "Hello, how are you?";
+	}
+	
+
+	public String getResponse(String statement)
+	{
+		String response = "";
+		
+		if (statement.length() == 0)
+		{
+			response = "Hello? any one there?";
+		}
+
+		else if (findKeyword(statement, "no") >= 0)
+		{
+			response = "Why so negative?";
+                	emotion--;
+		}
+		
+		else if (findKeyword(statement, "Folwell") >= 0)
+		{
+			response = "";
+			emotion++;
+		}
+
+		// Response transforming I want to statement
+		else if (findKeyword(statement, "I want to", 0) >= 0)
+		{
+			response = transformIWantToStatement(statement);
+		}
+		else if (findKeyword(statement, "I want",0) >= 0)
+		{
+			response = transformIWantStatement(statement);
+		}	
+		else
+		{
+			response = getRandomResponse();
+		}
+		
+		return response;
+	}
+	
+	
+	private String transformIWantToStatement(String statement)
+	{
+		//  Remove the final period, if there is one
+		statement = statement.trim();
+		String lastChar = statement.substring(statement
+				.length() - 1);
+		if (lastChar.equals("."))
+		{
+			statement = statement.substring(0, statement
+					.length() - 1);
+		}
+		int psn = findKeyword (statement, "I want to", 0);
+		String restOfStatement = statement.substring(psn + 9).trim();
+		return "Why do you want to " + restOfStatement + "?";
+	}
+
+	
+	private String transformIWantStatement(String statement)
+	{
+		//  Remove the final period, if there is one
+		statement = statement.trim();
+		String lastChar = statement.substring(statement
+				.length() - 1);
+		if (lastChar.equals("."))
+		{
+			statement = statement.substring(0, statement
+					.length() - 1);
+		}
+		int psn = findKeyword (statement, "I want", 0);
+		String restOfStatement = statement.substring(psn + 6).trim();
+		return "Would you really be happy if you had " + restOfStatement + "?";
+	}
+	
+	
+
+	private String transformIYouStatement(String statement)
+	{
+		//  Remove the final period, if there is one
+		statement = statement.trim();
+		String lastChar = statement.substring(statement
+				.length() - 1);
+		if (lastChar.equals("."))
+		{
+			statement = statement.substring(0, statement
+					.length() - 1);
+		}
+		
+		int psnOfI = findKeyword (statement, "I", 0);
+		int psnOfYou = findKeyword (statement, "you", psnOfI);
+		
+		String restOfStatement = statement.substring(psnOfI + 1, psnOfYou).trim();
+		return "Why do you " + restOfStatement + " me?";
+	}
+	
+
+
+	private int findKeyword(String statement, String goal,
+			int startPos)
+	{
+		String phrase = statement.trim().toLowerCase();
+		goal = goal.toLowerCase();
+
+		int psn = phrase.indexOf(goal, startPos);
+
+		while (psn >= 0)
+		{
+			// Find the string of length 1 before and after
+			// the word
+			String before = " ", after = " ";
+			if (psn > 0)
+			{
+				before = phrase.substring(psn - 1, psn);
+			}
+			if (psn + goal.length() < phrase.length())
+			{
+				after = phrase.substring(
+						psn + goal.length(),
+						psn + goal.length() + 1);
+			}
+
+		
+			if (((before.compareTo("a") < 0) || (before
+					.compareTo("z") > 0)) // before is not a
+											// letter
+					&& ((after.compareTo("a") < 0) || (after
+							.compareTo("z") > 0)))
+			{
+				return psn;
+			}
+
+			// The last position didn't work, so let's find
+			// the next, if there is one.
+			psn = phrase.indexOf(goal, psn + 1);
+
+		}
+
+		return -1;
+	}
+	
+
+	private int findKeyword(String statement, String goal)
+	{
+		return findKeyword (statement, goal, 0);
+	}
+	
+
+
+	private String getRandomResponse ()
+	{
+		Random r = new Random ();
+		if (emotion == 0)
+		{	
+			return randomNeutralResponses [r.nextInt(randomNeutralResponses.length)];
+		}
+		if (emotion < 0)
+		{	
+			return randomAngryResponses [r.nextInt(randomAngryResponses.length)];
+		}	
+		return randomHappyResponses [r.nextInt(randomHappyResponses.length)];
+	}
+	
+	private String [] randomNeutralResponses = {"Interesting, tell me more",
+			"Hmmm.",
+			"Do you really think so?",
+			"You don't say.",
+			"It's all boolean to me.",
+			"So, would you like to go for a walk?",
+			"Could you say that again?"
+	};
+	private String [] randomAngryResponses = {"Bahumbug.", "Harumph", "The rage consumes me!"};
+	private String [] randomHappyResponses = {"H A P P Y, what's that spell?", "Today is a good day", "You make me feel like a brand new pair of shoes."};
+	
+}
